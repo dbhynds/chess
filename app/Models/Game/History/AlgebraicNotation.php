dcfddca4bce6b73748db2f9c0f0ab46377bbede3
Davo Hynds <dbhynds@gmail.com>
2024-04-20 12:23:59 -0500

    begin working on regex for algebraic notation

--- app/Models/Game/History/AlgebraicNotation.php
+++ app/Models/Game/History/AlgebraicNotation.php
@@ -4,15 +4,26 @@ namespace App\Models\Game\History;
 
 class AlgebraicNotation
 {
-    private $spaceRegex = '[a-h][1-8]';
+    public const rank = '[1-8]';
 
-    private $majorPieceRegex = '[RKBQK]';
+    public const file = '[a-h]';
 
-    private $capturesRegex = 'x';
+    public const space = self::file.self::rank;
 
-    private $checksRegex = '[\!\#]';
+    public const piece = '[RKBQK]{0,1}';
 
-    private $promotesRegex = '=';
+    public const captures = 'x{0,1}';
 
-    private $castleRegex = '0(-0){1,2}';
+    public const checks = '[\!\#]';
+
+    public const promotes = '=';
+
+    public const castle = 'O(-O){1,2}';
+
+    public const regex = '^'.self::piece.self::captures.self::space.'$';
+
+    public static function isValid(string $notation): bool
+    {
+        return preg_match(self::regex, $notation) > 0;
+    }
 }
